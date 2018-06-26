51e70082350753702e8d3d9dc9ed18edb7283078
Shannon Adair <shan@Shannons-MacBook-Pro.local>
2018-06-26 15:42:12 -0600

    scheduler start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './reset.css'
 import './App.css';
+import Schedule from './Components/Schedule/Schedule'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         It's MEDTEL!!!
+          <Schedule />
       </div>
     );
   }
