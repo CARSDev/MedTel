5a48122a8827444470d9cb92c53070e24be2d082
Rachel Hicken <rachkh22@gmail.com>
2018-06-26 17:25:22 -0600

    minor changes for testing

--- src/Components/PatientList/PatientList.js
+++ src/Components/PatientList/PatientList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Link from 'react-router-dom';
+// import Link from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
