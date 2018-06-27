595c8b25882ec31e2bb9179583c8a2fe48415275
Rachel Hicken <rachkh22@gmail.com>
2018-06-27 12:09:23 -0600

    setup all patient endpoint

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,7 +1,8 @@
 const addAuthEndpointsTo = require('./controllers/auth');
+const addRachelEndpointsTo = require('./controllers/rachel_controller');
 
 module.exports = function addAllEndpointsTo(app) {
     //AUTHENTICATION
     addAuthEndpointsTo(app)
-
+    addRachelEndpointsTo(app)
 }
