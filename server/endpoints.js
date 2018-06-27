0bc58592a87a6c63e4bd4664dfb32423a5eab6bd
Drew Bloomfield <DrewBloomfield@Drews-MacBook-Air.local>
2018-06-27 11:54:05 -0600

    add a drew endpoint

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,7 +1,11 @@
 const addAuthEndpointsTo = require('./controllers/auth');
+const addDrewEndpointsTo = require('./controllers/drew');
 
 module.exports = function addAllEndpointsTo(app) {
     //AUTHENTICATION
     addAuthEndpointsTo(app)
+
+    //DREW
+    addDrewEndpointsTo(app)
 
 }
