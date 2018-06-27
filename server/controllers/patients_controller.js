8382bdcda36e3f10e7d113a7fc705b139099ec8f
Rachel Hicken <rachkh22@gmail.com>
2018-06-27 10:34:17 -0600

    removed controller to avoid conflicts

--- server/controllers/patients_controller.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    all_patients: (req, res, next) => {
-        const dbInstance = req.app.get('db');
-        dbInstance.all_patients()
-        .then((patients)=>res.status(200).send(patients))
-        .catch((e) => { console.log(e); res.status(500).send("Couldn't get all_patients")});
-    }
-}
