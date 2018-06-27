be5e620a96b447a899434b3a2f98cc8282c79518
Rachel Hicken <rachkh22@gmail.com>
2018-06-27 10:36:53 -0600

    removed test component from app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import PatientList from './Components/PatientList/PatientList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         It's MEDTEL!!!
-        <PatientList/>
       </div>
     );
   }
