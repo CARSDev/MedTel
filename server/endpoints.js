39b1c38c728b10672a54fc43343ee2b6ede1a068
Shannon Adair <shan@Shannons-MacBook-Pro.local>
2018-06-27 16:39:41 -0600

    scheduling times

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,7 +1,8 @@
 const addAuthEndpointsTo = require('./controllers/auth');
+const addShanEndpointsTo = require('./controllers/shan')
 
 module.exports = function addAllEndpointsTo(app) {
     //AUTHENTICATION
     addAuthEndpointsTo(app)
-
+    addShanEndpointsTo(app)
 }
--- src/Components/Schedule/Day.js
+++ src/Components/Schedule/Day.js
@@ -1,35 +1,48 @@
 import React, {Component} from 'react';
 import './Day.css'
+import moment from 'moment'
 
 export default class Day extends Component{
     constructor(){
         super()
         this.state={
-
+            times: [],
+            patients: [],
+            reasons: []
         }
     }
 
-    render(){
+    componentDidMount() {
+        var d = this.props.morning
+        var timeStops = [];
+        var amTime = moment(d).add(8, 'h')._d
+        for (let i = 0; i < 10; i++){
+            timeStops.push(amTime)
+            amTime=moment(amTime).add(30, 'm')._d
+        }
+
+        var pmTime = moment(d).add(13, 'h')._d
+        for (let i = 0; i < 8; i++) {
+            timeStops.push(pmTime)
+            pmTime = moment(pmTime).add(30, 'm')._d
+        }
+        this.setState({ times: timeStops })
+    }
+
+    render() {
+        console.log(this.props.date)
+        console.log(this.props.morning)
+        const {times} = this.state
+
         return(
             <div className= 'dayList'>
                 <div className='timesColumn'>
                     <h1>Appointment Time</h1>
-                    <h3>8:00am</h3>
-                    <h3>8:30am</h3>
-                    <h3>9:00am</h3>
-                    <h3>9:30am</h3>
-                    <h3>10:00am</h3>
-                    <h3>10:30am</h3>
-                    <h3>11:00am</h3>
-                    <h3>11:30am</h3>
-                    <h3>1:00pm</h3>
-                    <h3>1:30pm</h3>
-                    <h3>2:00pm</h3>
-                    <h3>2:30pm</h3>
-                    <h3>3:00pm</h3>
-                    <h3>4:00pm</h3>
-                    <h3>5:00pm</h3>
-                    <h3>5:30pm</h3>
+                    {times.map((val, i) => {
+                        return (
+                            <h3 key={i} >{moment(val).format("h:mm A")}</h3>
+                        )
+                    })}
                 </div>
                 <div className="namesColumn">
                     <h1>Patient Name</h1>
