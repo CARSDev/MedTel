b434f79060680e5dd66cc459f6206a00ad16921b
Drew Bloomfield <DrewBloomfield@Drews-MacBook-Air.local>
2018-06-27 16:47:54 -0600

    Build most of login page.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 //Components
 import Nav from './Components/Nav/Nav'
+import Login from './Components/Login/Login'
 import './reset.css'
 import './App.css';
 import routes from './routes'
+import { withRouter } from "react-router-dom";
+
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Nav/>
-        {routes}
-      </div>
+
+      (this.props.location.pathname === '/') ? 
+      
+        <div className="App">
+          <Login/>  
+        </div>
+        
+      :
+        
+        <div className="App">
+          <Nav />
+          {routes}
+        </div>
+      
     );
   }
 }
 
-export default App;
+export default withRouter(App);
