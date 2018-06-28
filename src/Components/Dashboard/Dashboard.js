4bd71463e3be0ab4b2495e2a286a5e305a0183f2
Rachel Hicken <rachkh22@gmail.com>
2018-06-28 12:09:55 -0600

    added results basic layout to the dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 
 import React, { Component } from 'react'
 import PatientInfo from './PatientInfo/PatientInfo'
+import Results from './Results/Results';
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
-        <PatientInfo/>
+        <PatientInfo />
+        <Results/>
       </div>
     )
   }
