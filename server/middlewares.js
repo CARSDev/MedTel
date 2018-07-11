fdc673b21893a7acea752a6f4a21d084d6dcdf64
Drew Bloomfield <DrewBloomfield@Drews-MacBook-Air.local>
2018-07-11 14:04:33 -0600

    turn off the super user

--- server/middlewares.js
+++ server/middlewares.js
@@ -40,21 +40,21 @@ exports.default = function addMiddlewaresTo(app) {
     });
     
     //SUPERUUUUUUUSER!!!
-    app.use((req, res, next) => {
-        req.user = {
-            employee_id: 2,
-            company_id: 1,
-            employee_full_name: "Donna Schmiphflingherr",
-            employee_first_name: "Donna",
-            employee_last_name: "Schmiphflingherr",
-            employee_picture: 'https://img.etsystatic.com/il/8f3c6d/818346854/il_570xN.818346854_r3b9.jpg?version=0',
-            role_id: 1,
-            employee_hashed_password: "",
-            employee_email: "degs@gmail.com",
-            employee_username: "Degs"
-        }
-        next()
-    });
+    // app.use((req, res, next) => {
+    //     req.user = {
+    //         employee_id: 2,
+    //         company_id: 1,
+    //         employee_full_name: "Donna Schmiphflingherr",
+    //         employee_first_name: "Donna",
+    //         employee_last_name: "Schmiphflingherr",
+    //         employee_picture: 'https://img.etsystatic.com/il/8f3c6d/818346854/il_570xN.818346854_r3b9.jpg?version=0',
+    //         role_id: 1,
+    //         employee_hashed_password: "",
+    //         employee_email: "degs@gmail.com",
+    //         employee_username: "Degs"
+    //     }
+    //     next()
+    // });
 
 }
 
